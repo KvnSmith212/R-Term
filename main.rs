25d99e49c19836678a96362a5e936c8b269f388b
Kevin Smith <kvnsmith212@gmail.com>
2016-06-11 23:29:19 -0400

    added readline functionality.

--- main.rs
+++ main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
+macro_rules! printfl {
+        // need to read up on rust macros, have no idea what's going on with: ($($tt:tt)*)
+        ($($tt:tt)*) => {{
+        use std::io::Write;
+        print!($($tt)*);
+        ::std::io::stdout().flush().unwrap();
+    }}
+}
+
 fn main() {
     
     //Load config files
@@ -11,8 +22,9 @@ fn main() {
 fn run_loop() {
 
     loop {
-        println!("> ");
-        let input : String = read_line();
+        printfl!("> ");
+
+        let input : io::Result<()> = read_line();
         let args : Vec<String> = split_args(input);
         let success : bool = execute(args);
 
@@ -22,13 +34,18 @@ fn run_loop() {
     }
 }
 
-fn read_line() -> String {
-    let line : String = "arg1 arg2 arg3".to_string();
-    line
+fn read_line() -> io::Result<()> {
+    let mut input = String::new();
+
+    try!(io::stdin().read_line(&mut input));
+
+    //can I get just the string to return. SHOULD I return just the string?
+    Ok(())
 }
 
-fn split_args(input : String) -> Vec<String> {
+fn split_args(input : io::Result<()>) -> Vec<String> {
     let args : Vec<String> = vec!["arg1".to_string(), "arg2".to_string(), "arg3".to_string()];
+
     args
 }
 

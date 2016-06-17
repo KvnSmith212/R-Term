f41bb76d7061780a94479b3ff168771d72918cec
Kevin Smith <kvnsmith212@gmail.com>
2016-06-16 22:33:36 -0400

    refactored getting input and filled out split_args method

--- main.rs
+++ main.rs
@@ -24,8 +24,8 @@ fn run_loop() {
     loop {
         printfl!("> ");
 
-        let input : io::Result<()> = read_line();
-        let args : Vec<String> = split_args(input);
+        let input : String = handle_read();
+        let args : Vec<String> = split_args(&input);
         let success : bool = execute(args);
 
         if !success {
@@ -34,21 +34,39 @@ fn run_loop() {
     }
 }
 
-fn read_line() -> io::Result<()> {
-    let mut input = String::new();
-
-    try!(io::stdin().read_line(&mut input));
-
-    //can I get just the string to return. SHOULD I return just the string?
-    Ok(())
+fn handle_read() -> String {
+    match read_line() {
+        Err(e) => panic!("{:?}", e),
+        Ok(value) => value,
+    }
 }
 
-fn split_args(input : io::Result<()>) -> Vec<String> {
-    let args : Vec<String> = vec!["arg1".to_string(), "arg2".to_string(), "arg3".to_string()];
+fn read_line() -> io::Result<String> {
+    let mut input = String::new();
+    try!(io::stdin().read_line(&mut input));
+    Ok(input)
+}
+
+/*
+    Split input into args with the form: command arg1 arg2 arg3...
+    Quotes, multiple lines, etc all ignore for now.
+*/
+fn split_args(input : &str) -> Vec<String> {
+    let split = input.split(" ");
+    let temp_args: Vec<&str> = split.collect();
+    let mut args = Vec::new();
+    
+    for arg in temp_args {
+        args.push(arg.to_string());
+    }
 
     args
 }
 
 fn execute(args : Vec<String>) -> bool {
+    for arg in args {
+        println!("args: {}", arg)
+    }
+    
     false
 }
